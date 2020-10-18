aac48ac1f48227769a219f2e445c33116114bbfe
michaljk1 <theNeweSTls1>
2020-10-18 18:06:27 +0200

    temp id on username

--- src/main/kotlin/com/ruczajsoftware/workoutrival/model/database/User.kt
+++ src/main/kotlin/com/ruczajsoftware/workoutrival/model/database/User.kt
@@ -2,12 +2,13 @@ package com.ruczajsoftware.workoutrival.model.database
 
 import com.arangodb.springframework.annotation.Document
 import com.thinkinglogic.builder.annotation.Builder
+import org.springframework.data.annotation.Id
 import java.util.ArrayList
 
 @Builder
 @Document("Users")
 class User(
-    var username: String = "",
+    @Id var username: String = "",
     var password: String = "",
     var email: String = "",
     var personalData: PersonalData = PersonalData(),
